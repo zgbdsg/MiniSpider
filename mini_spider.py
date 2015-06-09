8b7b320096d07f7ea27f875cb977b982f9693ab8
zgb <zgbdsg@gmail.com>
2015-06-09 16:13:02 +0800

    zgb finish

--- mini_spider.py
+++ mini_spider.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
+# !/usr/bin/env python
+################################################################################
+#
+# Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+
+"""
+This is the main module
+"""
 __author__ = 'zhouguobing@baidu.com'
 import ConfigParser
-import url
-import log
 import logging
-import threadpool
 import urllib2
 import time
-import chardet
 import re
+from StringIO import StringIO
+import gzip
+
+import chardet
 from bs4 import BeautifulSoup
 
-class MiniSpider():
+import url
+from util import threadpool, log
 
+
+class MiniSpider():
     def __init__(self, config, seed):
         log.init_log('./log/MiniSpider')
 
@@ -22,22 +35,23 @@ class MiniSpider():
         self.conf_dic = dict(cp._sections)
         for key in self.conf_dic:
             self.conf_dic[key] = dict(cp._defaults, **self.conf_dic[key])
-            self.conf_dic[key].pop('__name__',None)
+            self.conf_dic[key].pop('__name__', None)
 
         urllib2.socket.setdefaulttimeout(float(self.conf_dic['spider']['crawl_timeout']))
         self.seeds = ["http://pycm.baidu.com:8081/page3.html"]
+        # self.seeds = ["http://www.sina.com.cn/"]
 
         self.urls = []
         self.urlpool = {}
         for i in range(len(self.seeds)):
-            u = url.Url(self.seeds[i],"/")
+            u = url.Url(self.seeds[i], "")
             self.urls.append(u)
             self.urlpool[u.link] = 1
 
         logging.info("init")
 
     def run(self):
-        #self.pool = Pool(processes=8)
+        # self.pool = Pool(processes=8)
         self.pool = threadpool.ThreadPool(int(self.conf_dic['spider']['thread_count']))
         requests = threadpool.makeRequests(self.crawl, self.urls, self.print_result, self.handle_exception)
 
@@ -61,15 +75,21 @@ class MiniSpider():
 
         try:
             page = urllib2.urlopen(url.link)
-            chunk = page.read()
-            #print page.info()
+            if page.info().get('Content-Encoding') == 'gzip':
+                buf = StringIO(page.read())
+                f = gzip.GzipFile(fileobj=buf)
+                chunk = f.read()
+            else:
+                chunk = page.read()
+            # print page.info()
             # print chunk
             result = re.match(self.conf_dic['spider']['target_url'], url.link)
             if not result == None:
-                self.savepage(url,chunk)
+                self.savepage(url, chunk)
 
             if page.info()['Content-Type'] in ["text/html"]:
                 self.processpage(chunk, url)
+
         except urllib2.HTTPError, e:
             print e.code
         except urllib2.URLError, e:
@@ -81,12 +101,16 @@ class MiniSpider():
 
     def processpage(self, chunk, purl):
         encode = chardet.detect(chunk)['encoding']
-        if encode in ['ascii','utf-8']:
+        # print encode
+        if encode in ['ascii', 'utf-8']:
             pass
-        elif encode in ['gbk']:
+        elif encode in ['GBK']:
             chunk.decode('gbk', 'ignore').encode('utf-8')
-        elif encode in ['gb18030']:
-            chunk.decode('gb18030', 'ignore').encode('utf-8')
+        elif encode in ['GB2312']:
+            # print "GB2312"
+            chunk.decode('GB2312', 'ignore').encode('utf-8')
+        elif encode in ['GB18030']:
+            chunk.decode('GB18030', 'ignore').encode('utf-8')
 
         soup = BeautifulSoup(chunk)
         self.extractlink(soup, purl)
@@ -95,31 +119,36 @@ class MiniSpider():
         hrefs = []
         links1 = soup.findAll("a")
         for link in links1:
-            href = link.get('href').strip('/')
-            if 'javascript:location.href' in href:
-                href = href[len("javascript:location.href")+2:-2]
+            href = link.get('href')
+            if href:
+                href = href.strip("/")
+            # if 'javascript:location.href' in href:
+            #     href = href[len("javascript:location.href")+2:-2]
             hrefs.append(href)
 
         links2 = soup.find_all("img")
         for link in links2:
-            href = link.get('src').strip('/')
-            hrefs.append(href)
+            href = link.get('src')
+            if href:
+                hrefs.append(href)
 
         for href in hrefs:
-            u = url.Url(purl.seed,purl.relpath+"/"+href)
+            # u = url.Url(purl.seed,purl.relpath+"/"+href)
+            u = url.Url(purl.link, href)
             if u.depth > int(self.conf_dic['spider']['max_depth']):
                 continue
             if u.link not in self.urlpool:
                 self.urlpool[u.link] = 1
-                req = threadpool.WorkRequest(self.crawl,[u],callback=self.print_result, exc_callback=self.handle_exception)
-                #print(newurl)
+                req = threadpool.WorkRequest(self.crawl, [u], callback=self.print_result,
+                                             exc_callback=self.handle_exception)
+                # print(newurl)
                 self.pool.putRequest(req)
                 print "new url  %s added." % u.link
 
     def savepage(self, url, chuck):
         name = urllib2.quote(url.link)
         result, number = re.subn(re.compile('["\\/*?<>|]'), "", name)
-        f = open("output/"+result,"wb")
+        f = open("output/" + result, "wb")
         f.write(chuck)
         f.flush()
         f.close()
@@ -138,8 +167,9 @@ class MiniSpider():
             print exc_info
             raise SystemExit
         print "**** Exception occured in request #%s: %s" % \
-          (request.requestID, exc_info)
+              (request.requestID, exc_info)
+
 
 if __name__ == "__main__":
-    ms = MiniSpider("spider.conf",None)
+    ms = MiniSpider("spider.conf", None)
     ms.run()
--- url.py
+++ url.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
-__author__ = 'zhouguobing'
+# !/usr/bin/env python
+################################################################################
+#
+# Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+
+"""
+This is the url module
+"""
+__author__ = 'zhouguobing@baidu.com'
 
 from urlparse import urlparse
+from urlparse import urljoin
 
 class Url():
     def __init__(self, seed, relativepath):
-        link = seed.strip('/')+"/"+relativepath.strip('/')
-        obj = urlparse(link.strip("/"))
+        #link = seed.strip('/')+"/"+relativepath.strip('/')
+        #obj = urlparse(link.strip("/"))
+        dest = urljoin(seed.strip("/"),relativepath)
+        obj = urlparse(dest)
         self.seed = "%s://%s"%(obj.scheme, obj.netloc)
         # self.seed = obj.scheme+"://"+obj.netloc+":"+obj.port
         self.link = obj.geturl()
@@ -17,11 +30,11 @@ class Url():
         self.relpath = "/".join(items[0:-1])
 
 if __name__ == "__main__":
-    seeds = ["http://pycm.baidu.com:8081/"]
+    seeds = ["http://pycm.baidu.com:8081/page3.html"]
 
     urls = []
     for i in range(len(seeds)):
-        u = Url(seeds[i],"/mirror/mirror/mirror/index.html")
+        u = Url(seeds[i],"")
         print u.link
         print u.netloc
         print u.path
